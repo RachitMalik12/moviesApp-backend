9bdd4b163624184000df9316760186aa167aa8a4
RachitMalik12 <rachitmalik1947@gmail.com>
2019-09-08 22:30:06 -0700

    Adding genres API initial work:

--- genres.js
+++ genres.js
@@ -1,14 +1,15 @@
 const express = require('express'); 
+const Joi = require('joi'); 
 const app = express(); 
 app.use(express.json()); 
 
 const genres = [
-    {id: "1", name: "Action",
-     id: '2', name: 'Horror',
-      id: '3', name: 'Adventure'
-      id: '4', name: 'Thriller'
-      id: '5', name: 'Murder Mystery' 
-    }
+    {id: "1", name: "Action"},
+    {id: '2', name: 'Horror'},
+    {id: '3', name: 'Adventure'}, 
+    {id: '4', name: 'Thriller'},
+    {id: '5', name: 'Murder Mystery'}
+    
 
 ]; 
 
@@ -16,6 +17,18 @@ app.get('/api/genres', (req,res) => {
     res.send(genres); 
 })
 
+app.get('/api/genres/:id', (req,res) => {
+    const genre = genres.findOne(genre => genre.id === parseInt(req.params.id)); 
+    if (!genre) res.status(404).message('Sorry, genre not found'); 
+   
+    res.send(genre); 
+})
 
-const port = process.env.PORT || 3000; 
-app.listen(port, () => console.log(`Running fast on port {$port}`); 
+const validateRequest(genre) {
+    const schema = {
+
+    }
+}
+
+const port = process.env.PORT || 4000; 
+app.listen(port, () => console.log('Running fast on port ' + port)); 
